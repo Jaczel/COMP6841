41433d139f5a8303d5660e7f75beff5de48aa863
Jaczel <jaczel@fastmail.com.au>
2020-03-18 17:50:05 +1100

    Cleaned up code and improved documentation

--- otp.py
+++ otp.py
@@ -1,5 +1,16 @@
 """
-Code to export
+This code supplements my blog post discussing One Time Pads:
+https://www.openlearning.com/u/josephaczel-q5v7qm/blog/
+Code to help understand how decoding an OTP can be used to decrypt other
+messages that use the same OTP.
+Example usage:
+String 1: LpaGbbfctNiPvwdbjnPuqolhhtygWhEuafjlirfPxxl
+String 3: UijMltDjeumxUnbiKstvdrVhcoDasUlrvDypegublg
+
+The first three characters of String 1 could possibly be the word 'The'.
+The first three characters of String 3 could be another word determined
+by subtracting the OTP found from the first three letters of string 1.
+
 - Joe Aczel
 18/3/2020
 """
@@ -31,13 +42,16 @@ otp_nums = dict(merge(numbers, letters))
 
 
 if __name__ == "__main__":
-    crypt = input("Enter part of the encrypted text you think you can decode: ")
-    plain = input("Enter what you think the text maps to: ")
+    crypt = input("Enter part of the encrypted text you think " +
+                  "you can decode (e.g. Lpa): ")
+    plain = input("Enter what you think the text maps to (e.g. The): ")
     print("Plain text expected is", plain)
     print("Encrypted text is", crypt)
     otp = decrypt(plain.lower(), crypt.lower())
     print("Great! Your OTP is", otp)
-    other_string = input("Enter some other encrypted text: ")
+    other_string = input("Enter text from another encrypted " +
+                         "string with the letters in the same " + 
+                         "place as the first(e.g. string 3, 'Uij': ")
 
     print("Subtracting", otp, "from", other_string)
 
